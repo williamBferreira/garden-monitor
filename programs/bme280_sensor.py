0bfa1b5920eb5d9e2550c1861fc14c87f60a0289
William Ferreira <williamferreira@Williams-MacBook-Pro.local>
2022-03-15 17:23:30 +0100

    log

--- programs/bme280_sensor.py
+++ programs/bme280_sensor.py
@@ -8,10 +8,10 @@ bus = smbus2.SMBus(port)
 
 bme280.load_calibration_params(bus,address)
 
-while True:
+read_all():
     bme280_data = bme280.sample(bus,address)
     humidity  = bme280_data.humidity
     pressure  = bme280_data.pressure
     ambient_temperature = bme280_data.temperature
     print(humidity, pressure, ambient_temperature)
-    sleep(1)
+    return (humidity, pressure, ambient_temperature)
