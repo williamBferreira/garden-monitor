7d0cfac3e34b6f2908ff37913584153b1e6a1df3
William Ferreira <williamferreira@Williams-MacBook-Pro.local>
2022-03-16 07:56:15 +0100

    add database

--- programs/bme280_sensor.py
+++ programs/bme280_sensor.py
@@ -1,6 +1,12 @@
 import bme280
 import smbus2
+import database
 from time import sleep
+
+
+db = database.weather_database() #Local MySQL db
+
+
 
 port = 1
 address = 0x77 # Adafruit BME280 address. Other BME280s may be different
@@ -13,3 +19,7 @@ humidity  = bme280_data.humidity
 pressure  = bme280_data.pressure
 ambient_temperature = bme280_data.temperature
 print(humidity, pressure, ambient_temperature)
+
+print("Inserting...")
+db.insert(0, temperature, 0, pressure, humidity,0)
+print("done")
